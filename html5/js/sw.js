1a7daaafbe41ed28efd6718e64c08c87a62f83e6
nobuhiko1107 <30885707+nobuhiko1107@users.noreply.github.com>
2017-08-28 15:45:41 +0900

    new10

--- html5/js/sw.js
+++ html5/js/sw.js
@@ -1,26 +1,13 @@
-self.addEventListener("push", function(event) {
-  event.waitUntil(
-    self.registration.pushManager.getSubscription()
-      .then(function(subscription) {
-        if (subscription) {
-          return subscription.endpoint
-        }
-        throw new Error('User not subscribed')
-    })
-    .then(function(res) {
-      return fetch('./notifications.json')
-    })
-    .then(function(res) {
-      if (res.status === 200) {
-        return res.json()
-      }
-      throw new Error('notification api response error')
-    })
-    .then(function(res) {
-      return self.registration.showNotification(res.title, {
-        icon: '/icon.png',
-        body: res.body
-      })
-    })
-  )
-})
+self.addEventListener("push", function(event){
+  event.waitUntil().then(function(res){
+    self.registration.showNotification("プッシュタイトル", {
+      body: "プッシュ通知",
+      icon: "/path/to.png",
+      tag: "tag"
+    });
+  });
+});
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  clients.openWindow("/");
+}, false);
